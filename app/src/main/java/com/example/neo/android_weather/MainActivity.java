08d64ad388d5b3279b10547712c8bc2b02c80919
mdctleo <31061195+mdctleo@users.noreply.github.com>
2018-09-07 11:36:53 -0700

    added retry policy

--- app/src/main/java/com/example/neo/android_weather/MainActivity.java
+++ app/src/main/java/com/example/neo/android_weather/MainActivity.java
@@ -5,17 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
+import com.android.volley.*;
 import com.android.volley.Response.ErrorListener;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private TextView weatherDisplay;
@@ -51,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         String url ="https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%2012590119&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+        StringRequest stringRequest = (StringRequest) new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -80,7 +76,11 @@ public class MainActivity extends AppCompatActivity {
             public void onErrorResponse(VolleyError error) {
                 weatherDisplay.setText("Something went wrong!");
             }
-        });
+        }).setRetryPolicy(new DefaultRetryPolicy(
+                DefaultRetryPolicy.DEFAULT_TIMEOUT_MS,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        ));
 
         queue.add(stringRequest);
 
